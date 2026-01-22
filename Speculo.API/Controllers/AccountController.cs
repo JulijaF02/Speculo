2541ff621f3a881611df73ad82ad3c94f86adf5a
Julija Filipović <111427175+JulijaF02@users.noreply.github.com>
2026-01-22 18:40:57 +0100

    feat: implement Docker, NUnit and Github actions
    * feat: Implement Event Sourcing core with LogMood and real identity tracking
    
    * Infrastructure: Added Docker, NUnit tests, and GitHub Actions CI
    
    * chore: add global.json to stabilize .net SDK version

--- Speculo.API/Controllers/AccountController.cs
+++ Speculo.API/Controllers/AccountController.cs
@@ -12,13 +12,12 @@ public class AccountController : ControllerBase
     [HttpGet("me")]
     public IActionResult GetMe()
     {
-        // Čitamo podatke direktno iz tokena (Claims)
         var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         var email = User.FindFirst(ClaimTypes.Email)?.Value;
         var fullName = User.FindFirst(ClaimTypes.Name)?.Value;
 
-        return Ok(new 
-        { 
+        return Ok(new
+        {
             Message = "Token is valid!",
             Id = userId,
             Email = email,
