77e3e826a6b7f4cf6e4fb3cb1eb5c6378b04a043
JulijaF02 <filipovicjuls@gmail.com>
2026-02-23 14:59:50 +0100

    test: complete unit test coverage for all command and query handlers

--- Speculo.Application.UnitTests/Features/Events/Queries/LogMoneyQueryHandlerTests.cs
+++
@@ -1,9 +0,0 @@
-using FluentAssertions;
-using NSubstitute;
-using Speculo.Application.Common.Interfaces;
-using Speculo.Application.Features.Events.Commands.LogMoney;
-using Speculo.Domain.Events;
-using Xunit;
-
-namespace Speculo.Application.UnitTests.Features.Events.Queries
-;
