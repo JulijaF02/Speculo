cd833bbae4d85ad4d65a65bd743fbb8513107ab3
JulijaF02 <filipovicjuls@gmail.com>
2026-05-02 21:58:42 +0200

    Migrate to May Azure subscription (eastus2, D2s_v3, new ACR)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -32,7 +32,7 @@ variable "subscription_id" {
 }
 
 variable "location" {
-  default = "West US 2"
+  default = "East US 2"
 }
 
 variable "resource_group_name" {
@@ -73,7 +73,7 @@ resource "azurerm_kubernetes_cluster" "speculo_aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B2s_v2"
+    vm_size    = "Standard_D2s_v3"
   }
 
   identity {
